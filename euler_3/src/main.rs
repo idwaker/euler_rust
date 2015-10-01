3009918349a7256f9afd5d080f7c9e547fb116fd
Diwaker Ghimire <idwaker@gmail.com>
2015-10-01 11:49:26 +0545

    completed project 3

---
+++ euler_3/src/main.rs
@@ -0,0 +1,55 @@
+// check if given number is prime
+fn is_prime(n: i64) -> bool {
+    if n == 2 {
+        return true;
+    }
+    if n < 2 || n % 2 == 0 {
+        return false;
+    }
+    let upto = (n as f64).sqrt() as i64;
+    for i in 3..upto {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
+struct Primes {
+    curr: i64
+}
+
+// ieterator for primes
+impl Iterator for Primes {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<i64> {
+
+        let mut next = self.curr;
+        while !is_prime(next) {
+            next += 1;
+        }
+        self.curr = next + 1;
+        Some(next)
+    }
+}
+
+fn factor(val: i64) -> i64 {
+    let primes = Primes {curr: 1};
+    if is_prime(val) { return val; }
+    for i in primes {
+        let quot = val as f64 / i as f64;
+        if val % i == 0 {
+            // so we have a factor
+            return factor(quot as i64);
+        }
+    }
+    return 0;
+}
+
+fn main() {
+    let value:i64 = 600851475143;
+
+    // find largest prime factor
+    println!("Factor: {}", factor(value));
+}
